b9ba969786e3cfd201eeb1090d858b020696b799
JensDiemer <git@jensdiemer.de>
2022-01-30 15:34:09 +0100

    Use darker and pytest-darker as code formatter + update requirements

--- django_yunohost_integration/dev_shell.py
+++ django_yunohost_integration/dev_shell.py
@@ -13,58 +13,14 @@ import django_yunohost_integration
 from django_yunohost_integration.local_test import create_local_test
 
 
-PACKAGE_ROOT = Path(django_yunohost_integration.__file__).parent.parent.parent
-BASE_PATH = Path(django_yunohost_integration.__file__).parent.parent
-
-
-def run_linters(cwd=None):
-    """
-    Run code formatters and linter
-    """
-    verbose_check_call(
-        'flake8',
-        cwd=cwd,
-        exit_on_error=True
-    )
-    verbose_check_call(
-        'isort', '--check-only', '.',
-        cwd=cwd,
-        exit_on_error=True
-    )
+PACKAGE_ROOT = Path(django_yunohost_integration.__file__).parent.parent
 
 
 class DevShellCommandSet(OriginDevShellCommandSet):
-
-    # TODO:
-    # pyupgrade --exit-zero-even-if-changed --py3-plus --py36-plus --py37-plus --py38-plus
-    # `find . -name "*.py" -type f ! -path "./.tox/*" ! -path "./htmlcov/*" ! -path "*/volumes/*"
-
-    def do_fix_code_style(self, statement: cmd2.Statement):
-        """
-        Fix code style by running: autopep8 and isort
-        """
-        verbose_check_call(
-            'autopep8', '--aggressive', '--aggressive', '--in-place', '--recursive', '.',
-            cwd=self.config.base_path
-        )
-        verbose_check_call(
-            'isort', '.',
-            cwd=self.config.base_path
-        )
-
-    def do_linting(self, statement: cmd2.Statement):
-        """
-        Linting: Check code style with flake8, isort and flynt
-        """
-        run_linters(cwd=self.config.base_path)
-
     def do_publish(self, statement: cmd2.Statement):
         """
         Publish "dev-shell" to PyPi
         """
-        # don't publish if code style wrong:
-        run_linters()
-
         # don't publish if test fails:
         verbose_check_call('pytest', '-x')
 
@@ -82,8 +38,8 @@ class DjangoYunoHostIntegrationCommandSet(DevShellBaseCommandSet):
         See README for details ;)
         """
         create_local_test(
-            django_settings_path=BASE_PATH / 'conf' / 'settings.py',
-            destination=BASE_PATH / 'local_test',
+            django_settings_path=PACKAGE_ROOT / 'conf' / 'settings.py',
+            destination=PACKAGE_ROOT / 'local_test',
             runserver=True,
         )
 
@@ -100,16 +56,14 @@ def get_devshell_app_kwargs():
     config = DevShellConfig(package_module=django_yunohost_integration)
 
     # initialize all CommandSet() with context:
-    kwargs = dict(
-        config=config
-    )
+    kwargs = dict(config=config)
 
     app_kwargs = dict(
         config=config,
         command_sets=[
             DjangoYunoHostIntegrationCommandSet(**kwargs),
             DevShellCommandSet(**kwargs),
-        ]
+        ],
     )
     return app_kwargs
 
