551551a6a080feb7ca8428f46566c4afc586ce83
JensDiemer <git@jensdiemer.de>
2022-08-24 10:03:48 +0200

    Update dev shell: Run a cmd2 App as CLI or shell

--- django_yunohost_integration/dev_shell.py
+++ django_yunohost_integration/dev_shell.py
@@ -1,8 +1,7 @@
-import sys
 from pathlib import Path
 
 import cmd2
-from dev_shell.base_cmd2_app import DevShellBaseApp
+from dev_shell.base_cmd2_app import DevShellBaseApp, run_cmd2_app
 from dev_shell.command_sets import DevShellBaseCommandSet
 from dev_shell.command_sets.dev_shell_commands import DevShellCommandSet as OriginDevShellCommandSet
 from dev_shell.config import DevShellConfig
@@ -74,5 +73,5 @@ def devshell_cmdloop():
     Entry point to start the "dev-shell" cmd2 app.
     Used in: [tool.poetry.scripts]
     """
-    c = DevShellApp(**get_devshell_app_kwargs())
-    sys.exit(c.cmdloop())
+    app = DevShellApp(**get_devshell_app_kwargs())
+    run_cmd2_app(app)  # Run a cmd2 App as CLI or shell
