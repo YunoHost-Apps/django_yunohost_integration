f1609229894bb747ceb2b03c343cc26fa11489cc
Jens Diemer <git@jensdiemer.de>
2024-08-04 19:40:31 +0200

    Apply manageprojects updates

--- django_yunohost_integration/tests/test_project_setup.py
+++ django_yunohost_integration/tests/test_project_setup.py
@@ -2,12 +2,12 @@ import subprocess
 from unittest import TestCase
 
 from bx_py_utils.path import assert_is_file
-from cli_base.cli_tools.code_style import assert_code_style
 from manageprojects.test_utils.project_setup import check_editor_config, get_py_max_line_length
+from cli_base.cli_tools.code_style import assert_code_style
 from packaging.version import Version
 
 from django_yunohost_integration import __version__
-from django_yunohost_integration.path_utils import get_project_root
+from django_yunohost_integration.cli_dev import PACKAGE_ROOT
 
 
 class ProjectSetupTestCase(TestCase):
@@ -17,18 +17,24 @@ class ProjectSetupTestCase(TestCase):
         version = Version(__version__)  # Will raise InvalidVersion() if wrong formatted
         self.assertEqual(str(version), __version__)
 
-        dev_cli_bin = get_project_root() / 'dev-cli.py'
+        cli_bin = PACKAGE_ROOT / 'cli.py'
+        assert_is_file(cli_bin)
+
+        output = subprocess.check_output([cli_bin, 'version'], text=True)
+        self.assertIn(f'django_yunohost_integration v{__version__}', output)
+
+        dev_cli_bin = PACKAGE_ROOT / 'dev-cli.py'
         assert_is_file(dev_cli_bin)
 
         output = subprocess.check_output([dev_cli_bin, 'version'], text=True)
         self.assertIn(f'django_yunohost_integration v{__version__}', output)
 
     def test_code_style(self):
-        return_code = assert_code_style(package_root=get_project_root())
+        return_code = assert_code_style(package_root=PACKAGE_ROOT)
         self.assertEqual(return_code, 0, 'Code style error, see output above!')
 
     def test_check_editor_config(self):
-        check_editor_config(package_root=get_project_root())
+        check_editor_config(package_root=PACKAGE_ROOT)
 
-        max_line_length = get_py_max_line_length(package_root=get_project_root())
+        max_line_length = get_py_max_line_length(package_root=PACKAGE_ROOT)
         self.assertEqual(max_line_length, 119)
